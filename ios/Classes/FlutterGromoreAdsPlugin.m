9f91c7c1fc4718666ad9acfcbaed74e09ea4c229
Zero <1300326388@qq.com>
2021-11-26 10:35:11 +0800

    1、初始化项目

---
+++ ios/Classes/FlutterGromoreAdsPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterGromoreAdsPlugin.h"
+
+@implementation FlutterGromoreAdsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_gromore_ads"
+            binaryMessenger:[registrar messenger]];
+  FlutterGromoreAdsPlugin* instance = [[FlutterGromoreAdsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
---
+++ lib/flutter_gromore_ads.dart
@@ -0,0 +1,14 @@
+
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+
+class FlutterGromoreAds {
+  static const MethodChannel _channel =
+      const MethodChannel('flutter_gromore_ads');
+
+  static Future<String> get platformVersion async {
+    final String version = await _channel.invokeMethod('getPlatformVersion');
+    return version;
+  }
+}
