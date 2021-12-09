d98afda05e43b91323232a37b8c5ae84877ba211
Zero <1300326388@qq.com>
2021-12-09 16:26:12 +0800

    迁移到 Flutter 2.5.3 版本

--- lib/flutter_gromore_ads.dart
+++ lib/flutter_gromore_ads.dart
@@ -6,8 +6,8 @@ class FlutterGromoreAds {
   static const MethodChannel _channel =
       const MethodChannel('flutter_gromore_ads');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 }
